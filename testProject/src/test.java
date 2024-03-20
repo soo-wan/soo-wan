6d5743d39c95b496997742aac5e67724aca48ee8
DESKTOP-F4BC13L\hi <ohsoowan@gmail.com>
2024-03-20 10:04:43 +0900

    first commit

---
+++ testProject/src/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요");
+        System.out.print("하이");
+        System.out.print("하이");
+        System.out.print("하이");
+        
+    }
+}
